b3bd59563acbea3d75bb355604e67857a52f11d1
JLDActuary <jldunn@verizon.net>
2018-12-31 11:16:45 -0500

    Initial Commit

---
+++ pareto_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pareto_test
+//
+//  Created by Joseph Dunn on 12/31/18.
+//  Copyright © 2018 Joseph Dunn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
